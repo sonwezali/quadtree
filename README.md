39d34b83687a55f5e6db8e8d98317a9f76e1cd1b
Ali Sonmez <al1.sonmez.mi@gmail.com>
2026-02-08 23:31:58 +0300

    Add visualization related features
    Just learned ElectronJS and tested a few things.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quadtree
+
+**Quadtree** is a data structure that partitions a two-dimensional space by recursively subdividing its regions into four. It is used in image processing and compression, spatial indexing, etc.
+
+For more, please refer to its [Wikipedia page](https://en.wikipedia.org/wiki/Quadtree).
---
+++ preload.js
@@ -0,0 +1,10 @@
+// this file is used only for learning purposes
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    ping: () => ipcRenderer.invoke('ping')
+})
