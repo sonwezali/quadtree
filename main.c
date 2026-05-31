1a91f97fed8225a910c8f1484082d02105346a42
Ali Sonmez <al1.sonmez.mi@gmail.com>
2026-05-31 08:43:19 +0300

    test from rpi

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main() {
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Hello from RPi!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
     }
